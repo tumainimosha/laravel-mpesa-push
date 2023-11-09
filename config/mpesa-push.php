a1c163745c454b342d5df123f3640c50a0016773
Tumaini Mosha <princeton.mosha@gmail.com>
2023-11-09 09:54:20 +0300

    Update mpesa-push.php

--- config/mpesa-push.php
+++ config/mpesa-push.php
@@ -5,7 +5,7 @@ return [
     /**
      * Request URL.
      */
-    'endpoint' => env('TZ_MPESA_PUSH_ENDPOINT', 'https://broker2.ipg.vodacom.co.tz:30010/iPG/b2c/ussd_push'),
+    'endpoint' => env('TZ_MPESA_PUSH_ENDPOINT'),
 
     /**
      * Path to Vodacom Certificate Authority (CA) Root certificate.
